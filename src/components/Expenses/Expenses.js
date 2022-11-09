dd1c10a5306886eded9819a8bedcbe8461480ba6
Greg Stickel <gregory.stickel@live.com>
2022-11-09 15:16:19 -0500

    Updated 3:16pm 11-09-2022

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {  useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card'
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter';
+
 
 function Expenses (props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+      setFilteredYear(selectedYear);
+    };
     
     return ( 
-      <Card className="expenses">
+      <div>
+       <Card className="expenses">
+      <ExpensesFilter  selected={filteredYear} onChangeFilter={filterChangeHandler}/>  
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -28,6 +37,7 @@ function Expenses (props) {
         date={props.items[3].date}
       />
       </Card>
+      </div>
   );
 }
 
